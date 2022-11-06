03ecd55a516efa73048aba4e5923108268682aa8
titoo1234 <49947010+titoo1234@users.noreply.github.com>
2022-11-06 11:14:21 +0100

    Add files via upload
    vzorec

---
+++ Vzorec/Models/__init__.py
@@ -0,0 +1,4 @@
+"""Flask app factory."""
+# this file might contain the app_factory later when the project becomes larger and there is more things to be done
+# before starting the server (check the api key, setup supported models etc., see
+# https://flask.palletsprojects.com/en/2.1.x/patterns/appfactories/)
