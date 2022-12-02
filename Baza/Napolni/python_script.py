899f02036a4a648eccf6e59f034e61e02b396988
marinkovicm <marko.marinkovic50@gmail.com>
2022-12-02 19:38:34 +0100

    zmešnjava ampak DELA NA DOCKERJU

---
+++ Baza/Napolni/python_script.py
@@ -0,0 +1,76 @@
+import pyodbc
+from sqlalchemy import create_engine, text
+from urllib.parse import quote_plus
+import sys
+import time
+from textwrap import dedent
+
+
+def create_db(server, port, uid, pwd, db_name):
+    '''
+        Creates new database on localhost, port 1433
+    '''
+    conn_str = "DRIVER={ODBC Driver 17 for SQL Server};Server=" + str(server) +";port=" + str(port)+";Database=master;UID="+str(uid)+";PWD="+str(pwd)+";"
+        #odbc_str = 'DRIVER={ODBC Driver 17 for SQL Server};' + self.conn_str
+        #you have to download odbc driver 17 https://www.microsoft.com/en-us/download/details.aspx?id=56567
+    full_conn_str = 'mssql+pyodbc:///?odbc_connect=' + quote_plus(conn_str)
+    engine = create_engine(full_conn_str)
+    engine = engine.execution_options(isolation_level="AUTOCOMMIT")
+    engine.execute(text(f"CREATE DATABASE {db_name}"))
+    engine.dispose()
+
+def fill_db(server, port, uid, pwd, db_name, sql_script):
+    conn_str = "DRIVER={ODBC Driver 17 for SQL Server};Server="+str(server)+";port="+str(port)+";Database="+str(db_name)+";UID="+ str(uid) +";PWD=" + str(pwd)+";"
+    # conn_str = dedent('''
+    #         Driver={driver};
+    #         Server=localhost;
+    #         Port={port};
+    #         Database={db_name};
+    #         Uid={username};
+    #         Pwd={password};
+    #     '''.format(driver="{ODBC Driver 17 for SQL Server}", server_name=server, db_name=db_name, username=uid, password=pwd, port=port))
+    # driver = "{ODBC Driver 17 for SQL Server}"
+    #     #server_name = "tcp:primerjava-cen.database.windows.net,1433"
+    # server_name = "localhost"
+    #     #db_name = "Primerjava_cen"
+    # db_name = "base123"
+    #     #username = "baza"
+    # username = "SA"
+    #     #password = "AdminAdmin1!"
+    # password = "yourStrong(!)Password"
+    # port = "1433"
+
+    # conn_str = dedent('''
+    #         Driver={driver};
+    #         Server={server_name};
+    #         Database={db_name};
+    #         Uid={username};
+    #         Pwd={password};
+    #         Port={port};
+    #     '''.format(driver=driver, server_name=server_name, db_name=db_name, username=username, password=password, port=port))
+    full_conn_str = 'mssql+pyodbc:///?odbc_connect=' + quote_plus(conn_str)
+    engine = create_engine(full_conn_str)
+    #engine = create_engine(conn_str)
+    #engine = pyodbc.connect(conn_str)
+    with open(sql_script) as file:
+        query = text(file.read())
+    engine = engine.execution_options(isolation_level="AUTOCOMMIT")
+    engine.execute(query)
+    engine.dispose()
+
+
+if __name__=='__main__':
+    time.sleep(30)
+    a = sys.argv[1]
+    b = sys.argv[2]
+    c = sys.argv[3]
+    d = sys.argv[4]
+    e = sys.argv[5]
+    f = sys.argv[6]
+    try:
+        create_db(a, b, c, d, e)
+        fill_db(a, b, c, d , e , f)
+    except Exception as e:
+        print(str(e))
+    #create_db("localhost", 1433, "SA", "yourStrong(!)Password", "base22")
+    #fill_db("localhost", 1433, "SA", "yourStrong(!)Password" ,"base22", "setup.sql")
--- Katalog/catalog.py
+++ Katalog/catalog.py
@@ -14,21 +14,28 @@ from flask_sqlalchemy import SQLAlchemy
 class Catalog(Resource):
     def get(self):
         
-        driver = "{ODBC Driver 17 for SQL Server}"
-        server_name = "tcp:primerjava-cen.database.windows.net,1433"
-        db_name = "Primerjava_cen"
-        username = "baza"
-        password = "AdminAdmin1!"
+        # driver = "{ODBC Driver 17 for SQL Server}"
+        # #server_name = "tcp:primerjava-cen.database.windows.net,1433"
+        # server_name = "Database"
+        # #če maš samo bazo na dockerju dap localhost
+        # #db_name = "Primerjava_cen"
+        # db_name = "base123"
+        # #username = "baza"
+        # username = "SA"
+        # #password = "AdminAdmin1!"
+        # password = "yourStrong(!)Password"
+        # port = "1433"
 
-        conn_str = dedent('''
-            Driver={driver};
-            Server={server_name};
-            Database={db_name};
-            Uid={username};
-            Pwd={password};
-            Encrypt=yes;
-            TrustServerCertificate=no;
-        '''.format(driver=driver, server_name=server_name, db_name=db_name, username=username, password=password))
+        # conn_str = dedent('''
+        #     Driver={driver};
+        #     Server={server_name};
+        #     Database={db_name};
+        #     Uid={username};
+        #     Pwd={password};
+        #     Port={port};
+        # '''.format(driver=driver, server_name=server_name, db_name=db_name, username=username, password=password, port=port))
+        #            Encrypt=yes;
+        #    TrustServerCertificate=no;
         #Connection Timeout=30;
         #conn_str = r"sqlite:///DRIVER={ODBC Driver 17 for SQL Server}C:/Users/Lenovo/Desktop/1faks/isrm1/racunalniske_storitve_v_oblaku/RSO-Primerjalnik-cen-izdelkov/testna_baza.db"
         #'mssql+pyodbc:///?odbc_connect=' + quote_plus("DRIVER={ODBC Driver 17 for SQL Server};Server=db;port=1433;DataBase=base1;UID=SA;PWD=yourStrong(!)Password;")
@@ -36,11 +43,19 @@ class Catalog(Resource):
         #conn_str = "mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 17 for SQL Server};Server=tcp:primerjava-cen.database.windows.net,1433;Database=Primerjava_cen;Uid=baza;Pwd=AdminAdmin1!;"
         #conn_str = "mssql+pyodbc:///?odbc_connect=" + quote_plus(conn_str)
         #query = "SELECT * from Trgovina"
-        query = "SELECT * FROM Trgovina"
+        server ="Database"
+        port = "1433"
+        uid= "SA"
+        pwd= "yourStrong(!)Password"
+        db_name = "base123"
+        conn_str = "DRIVER={ODBC Driver 17 for SQL Server};Server="+str(server)+";port="+str(port)+";Database="+str(db_name)+";UID="+ str(uid) +";PWD=" + str(pwd)+";"
+        query = "SELECT * FROM Trgovine"
         try:
             eng = SQLRepository(conn_str)
             eng.start_conn()
+            print("conn started success")
             result = eng.execute_query(query)
+            print("execute query succ")
             eng.close_conn()
             result = AppResult(True, "", result)
             return result.toJSON()
