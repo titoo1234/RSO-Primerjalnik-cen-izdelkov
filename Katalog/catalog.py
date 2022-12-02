aeeff659ac8e11fd8dc198e0030f26b90aa3f7f0
titoo1234 <tito.arnsek@gmail.com>
2022-12-02 11:43:04 +0100

    new update

--- Katalog/catalog.py
+++ Katalog/catalog.py
@@ -15,7 +15,7 @@ class Catalog(Resource):
     def get(self):
         
         driver = "{ODBC Driver 17 for SQL Server}"
-        server_name = "primerjava-cen.database.windows.net,1433"
+        server_name = "tcp:primerjava-cen.database.windows.net,1433"
         db_name = "Primerjava_cen"
         username = "baza"
         password = "AdminAdmin1!"
@@ -28,8 +28,8 @@ class Catalog(Resource):
             Pwd={password};
             Encrypt=yes;
             TrustServerCertificate=no;
-            Connection Timeout=30;
         '''.format(driver=driver, server_name=server_name, db_name=db_name, username=username, password=password))
+        #Connection Timeout=30;
         #conn_str = r"sqlite:///DRIVER={ODBC Driver 17 for SQL Server}C:/Users/Lenovo/Desktop/1faks/isrm1/racunalniske_storitve_v_oblaku/RSO-Primerjalnik-cen-izdelkov/testna_baza.db"
         #'mssql+pyodbc:///?odbc_connect=' + quote_plus("DRIVER={ODBC Driver 17 for SQL Server};Server=db;port=1433;DataBase=base1;UID=SA;PWD=yourStrong(!)Password;")
         #conn_str = "mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 17 for SQL Server};Server=tcp:primerjava-cen.database.windows.net,1433;Database=Primerjava_cen;Uid=baza;Pwd=AdminAdmin1!;Encrypt=yes;TrustServerCertificate=no;"
