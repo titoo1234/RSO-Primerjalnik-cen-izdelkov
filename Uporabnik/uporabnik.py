20bd592728c726a41418ebb407a04dd9d7630eba
titoo1234 <tito.arnsek@gmail.com>
2022-12-04 20:43:03 +0100

    vse ok

--- Uporabnik/uporabnik.py
+++ Uporabnik/uporabnik.py
@@ -10,33 +10,34 @@ from textwrap import dedent
 import pyodbc
 from flask_sqlalchemy import SQLAlchemy
 from Models.Exceptions import *
+import urllib.parse as up
+import psycopg2
 
 
 
 class Uporabnik(Resource):
     def get(self, id):
-        
-        driver = "{ODBC Driver 17 for SQL Server}"
-        server_name = "primerjava-cen.database.windows.net,1433"
-        db_name = "Primerjava_cen"
-        username = "baza"
-        password = "AdminAdmin1!"
 
-        conn_str = dedent('''
-            Driver={driver};
-            Server={server_name};
-            Database={db_name};
-            Uid={username};
-            Pwd={password};
-            Encrypt=yes;
-            TrustServerCertificate=no;
-            Connection Timeout=30;
-        '''.format(driver=driver, server_name=server_name, db_name=db_name, username=username, password=password))
-        #conn_str = "C:/Users/Lenovo/Desktop/1faks/isrm1/racunalniske_storitve_v_oblaku/RSO-Primerjalnik-cen-izdelkov/prva_baza.db"##################
-        query = f"SELECT Id,Ime,Admin FROM Uporabniki Where {id} = Id;"
-        eng = SQLRepository(conn_str)
-        eng.start_conn()
-        result = eng.execute_query(query)
-        eng.close_conn()
-        result = AppResult(True, "", result)
-        return result.toJSON()
+        
+
+        try:
+            query = f"SELECT Id,Ime,Admin FROM Uporabniki Where {id} = Id;"
+            up.uses_netloc.append("postgres")
+            url = up.urlparse("postgres://fzhvzwic:hjYYIyExOk4_UXtKv9BoWkqeso0gVhlB@peanut.db.elephantsql.com/fzhvzwic")
+            conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host='peanut.db.elephantsql.com', port=url.port )
+            conn.set_session(autocommit=True)
+            cur = conn.cursor()
+
+            cur.execute(query)
+            result = cur.fetchall()
+            # eng = SQLRepository(conn_str)
+            # eng.start_conn()
+            # print("conn started success")
+            
+            # result = eng.execute_query(query)
+            # print("execute query succ")
+            # eng.close_conn()
+            result = AppResult(True, "", result)
+            return result.toJSON()
+        except Exception as e:
+            return AppResult.create_error_result(str(e)).toJSON()
