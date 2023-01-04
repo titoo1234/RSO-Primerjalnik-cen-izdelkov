557f82982f841dae4d3cb347346ff7a2e60514d2
marinkovicm <marko.marinkovic50@gmail.com>
2023-01-04 15:15:20 +0100

    na uporabniku dodan health check(ni še readynessa) dodane metrike na uporabniku

--- Uporabnik/app.py
+++ Uporabnik/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_restful import Api#, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 
+from prometheus_flask_exporter import PrometheusMetrics
 
 #import logging
 
@@ -25,6 +26,19 @@ def create_app():
 
 #logging.basicConfig(filename='record.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 app = create_app()
+metrics = PrometheusMetrics(app, group_by="endpoint", path="/user/metrics")
+@app.route('/user/health')
+def health_uporabnik():
+    # tule bi načeloma mogli preverjat neki --_o_--
+    # na navodilih piše da moremo simulirat bolno storitev, to bi naredli mogoče tako,
+    # da bi meli neko globalno spremenljivo, ki bi se pri nekem določenem klicu spremenila,
+    # in botem bi tule prišlo do falsa
+    # neki simetričnega bi naredili za readiness pa še na eni mikrostoritvi
+    if 1 + 1 == 2:
+        return "Ok", 200
+    else:
+        return "Unhealthy", 500
+
 #db = SQLAlchemy(app)
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=5002)
--- Uporabnik/uporabnik.py
+++ Uporabnik/uporabnik.py
@@ -13,44 +13,61 @@ from Models.Exceptions import *
 import urllib.parse as up
 import psycopg2
 import pandas as pd
+#from threading import Lock
+#lock = Lock()
 
-
-
+from prometheus_client import Counter, Summary, Gauge
+requests_total = Counter('flask_http_requests_total', 'VSI KLICI NA /user/<id>', ['method', 'endpoint'])
+requests_summary = Summary('flask_http_requests_per_second', 'ŠTO NEVEM VEČ KA POMENI', ['method', 'endpoint'])
+#requests_gauge = Gauge('flask_http_request_latency_mean_seconds', 'POVPREČJE', ['method', 'endpoint'])
 
 class Uporabnik(Resource):
     def get(self, id=None):
+        requests_total.labels(method='GET', endpoint='/uporabnik').inc()
+        with requests_summary.labels(method="GET", endpoint="/uporabnik").time():
+        
+            if id is None:
+                try:
+                    query = f"SELECT Id,Ime FROM Uporabniki;"
+                    up.uses_netloc.append("postgres")
+                    url = up.urlparse("postgres://fzhvzwic:hjYYIyExOk4_UXtKv9BoWkqeso0gVhlB@peanut.db.elephantsql.com/fzhvzwic")
+                    conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host='peanut.db.elephantsql.com', port=url.port )
+                    conn.set_session(autocommit=True)
+                    # cur = conn.cursor()
+                    df = pd.read_sql_query(query, conn)
+                    result = df.to_dict("records")
+                    conn.close()
+                    result = AppResult(True, "", result)
 
-        if id is None:
-            try:
-                query = f"SELECT Id,Ime FROM Uporabniki;"
-                up.uses_netloc.append("postgres")
-                url = up.urlparse("postgres://fzhvzwic:hjYYIyExOk4_UXtKv9BoWkqeso0gVhlB@peanut.db.elephantsql.com/fzhvzwic")
-                conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host='peanut.db.elephantsql.com', port=url.port )
-                conn.set_session(autocommit=True)
-                # cur = conn.cursor()
-                df = pd.read_sql_query(query, conn)
-                result = df.to_dict("records")
-                conn.close()
-                result = AppResult(True, "", result)
-                return result.toJSON()
-            except Exception as e:
-                return AppResult.create_error_result(str(e)).toJSON()
+                    # with lock:
+                    #     requests_summary._lock
+                    #     count = requests_summary.labels(method='GET', endpoint='/uporabnik')._count
+                    #     summ = requests_summary.labels(method='GET', endpoint='/uporabnik')._sum
+                        
+                    #     if count > 0:
+                    #         requests_gauge.labels(method="GET", endpoint="/uporabnik").set(summ / count)
+                    #         requests_summary._lock.release
 
-        else:
-            try:
-                query = f"SELECT * FROM Uporabniki Where {id} = Id;"
-                up.uses_netloc.append("postgres")
-                url = up.urlparse("postgres://fzhvzwic:hjYYIyExOk4_UXtKv9BoWkqeso0gVhlB@peanut.db.elephantsql.com/fzhvzwic")
-                conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host='peanut.db.elephantsql.com', port=url.port )
-                conn.set_session(autocommit=True)
-                # cur = conn.cursor()
-                df = pd.read_sql_query(query, conn)
-                result = df.to_dict("records")
-                conn.close()
-                result = AppResult(True, "", result)
-                return result.toJSON()
-            except Exception as e:
-                return AppResult.create_error_result(str(e)).toJSON()
+
+                    return result.toJSON()
+                except Exception as e:
+                    return AppResult.create_error_result(str(e)).toJSON()
+
+            else:
+                try:
+                    query = f"SELECT * FROM Uporabniki Where {id} = Id;"
+                    up.uses_netloc.append("postgres")
+                    url = up.urlparse("postgres://fzhvzwic:hjYYIyExOk4_UXtKv9BoWkqeso0gVhlB@peanut.db.elephantsql.com/fzhvzwic")
+                    conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host='peanut.db.elephantsql.com', port=url.port )
+                    conn.set_session(autocommit=True)
+                    # cur = conn.cursor()
+                    df = pd.read_sql_query(query, conn)
+                    result = df.to_dict("records")
+                    conn.close()
+                    result = AppResult(True, "", result)
+                    return result.toJSON()
+                except Exception as e:
+                    return AppResult.create_error_result(str(e)).toJSON()
 
     def delete(self, id):
         try:
